81dc459b29a113a7b366b9f3718e366b9124ffb1
abhishek <abhishekmazumder69@gmail.com>
2020-10-08 20:48:44 +0530

    Above the fold section completed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Roboto':['Roboto', 'Arial', 'sans-serif']
+      },
+      spacing:{
+        '14':'3.5rem',
+        '18':'4.5rem',
+        '68': '20rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
